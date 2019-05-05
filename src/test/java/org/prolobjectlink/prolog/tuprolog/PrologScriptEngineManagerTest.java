f0ddd83c6349d1d0296dfdd82de932de81db08c9
Jose Zalacain <Jose Zalacain@10.11.0.30>
2019-05-05 16:32:25 -0400

    javax.script

--- src/test/java/org/prolobjectlink/prolog/tuprolog/PrologScriptEngineManagerTest.java
+++ src/test/java/org/prolobjectlink/prolog/tuprolog/PrologScriptEngineManagerTest.java
@@ -23,10 +23,7 @@ package org.prolobjectlink.prolog.tuprolog;
 
 import static org.junit.Assert.assertEquals;
 
-import java.util.List;
-
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineFactory;
 import javax.script.ScriptException;
 
 import org.junit.Test;
@@ -36,10 +33,15 @@ public class PrologScriptEngineManagerTest extends PrologBaseTest {
 	@Test
 	public void test() throws ScriptException {
 		javax.script.ScriptEngineManager manager = new javax.script.ScriptEngineManager();
-		List<ScriptEngineFactory> factories = manager.getEngineFactories();
-//		assertTrue(factories.contains(PrologScriptEngineFactory.DEFAULT_FACTORY));
+		ScriptEngine engine = manager.getEngineByName(provider.getName());
+		assertEquals(true, engine.eval("X is 5+3."));
+		assertEquals("8", engine.get("X"));
 
-		ScriptEngine engine = manager.getEngineByName(provider.newEngine().getName());
+		engine = manager.getEngineByName("Prolog");
+		assertEquals(true, engine.eval("X is 5+3."));
+		assertEquals("8", engine.get("X"));
+
+		engine = manager.getEngineByName("prolog");
 		assertEquals(true, engine.eval("X is 5+3."));
 		assertEquals("8", engine.get("X"));
 
