8eda53283d91ac76f9c9c73681977e53688c2e7a
Jose Zalacain <Jose Zalacain@10.11.0.30>
2019-05-10 15:29:11 -0400

    Rename some test cases

--- src/test/java/org/prolobjectlink/prolog/tuprolog/PrologEngineTest.java
+++ src/test/java/org/prolobjectlink/prolog/tuprolog/PrologEngineTest.java
@@ -336,7 +336,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testAssertaIPrologTerm() {
+	public final void testAssertaPrologTerm() {
 		engine.asserta(provider.newStructure(parent, pam, bob));
 		assertEquals(1, engine.getProgramSize());
 
@@ -363,7 +363,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testAssertaIPrologTermIPrologTermArray() {
+	public final void testAssertaPrologTermPrologTermArray() {
 		PrologVariable x = provider.newVariable("X", 0);
 		PrologVariable y = provider.newVariable("Y", 1);
 		engine.asserta(provider.newStructure(mother, x, y), provider.newStructure(parent, x, y),
@@ -392,7 +392,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testAssertzIPrologTerm() {
+	public final void testAssertzPrologTerm() {
 		engine.assertz(provider.newStructure(parent, pam, bob));
 		assertEquals(1, engine.getProgramSize());
 
@@ -419,7 +419,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testAssertzIPrologTermIPrologTermArray() {
+	public final void testAssertzPrologTermPrologTermArray() {
 		PrologVariable x = provider.newVariable("X", 0);
 		PrologVariable y = provider.newVariable("Y", 1);
 		engine.assertz(provider.newStructure(mother, x, y), provider.newStructure(parent, x, y),
@@ -448,7 +448,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testClauseIPrologTerm() {
+	public final void testClausePrologTerm() {
 		engine.assertz("parent( pam, bob)");
 		engine.assertz("parent( tom, bob)");
 		engine.assertz("parent( tom, liz)");
@@ -472,7 +472,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testClauseIPrologTermIPrologTermArray() {
+	public final void testClausePrologTermPrologTermArray() {
 		engine.assertz("parent( pam, bob)");
 		engine.assertz("parent( tom, bob)");
 		engine.assertz("parent( tom, liz)");
@@ -526,7 +526,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testRetractIPrologTerm() {
+	public final void testRetractPrologTerm() {
 		engine.assertz("parent( pam, bob)");
 		engine.assertz("parent( tom, bob)");
 		engine.assertz("parent( tom, liz)");
@@ -554,7 +554,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testRetractIPrologTermIPrologTermArray() {
+	public final void testRetractPrologTermPrologTermArray() {
 		engine.assertz("mother( X, Y):-parent( X, Y),female( X)");
 		assertEquals(1, engine.getProgramSize());
 
@@ -680,7 +680,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testFindIPrologTerm() {
+	public final void testFindPrologTerm() {
 
 		engine.assertz("parent( pam, bob)");
 		engine.assertz("parent( tom, bob)");
@@ -714,7 +714,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testFindIPrologTermArray() {
+	public final void testFindPrologTermArray() {
 
 		engine.assertz("parent( pam, bob)");
 		engine.assertz("parent( tom, bob)");
@@ -808,7 +808,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testFindAllIPrologTerm() {
+	public final void testFindAllPrologTerm() {
 
 		engine.assertz("parent( pam, bob)");
 		engine.assertz("parent( tom, bob)");
@@ -865,7 +865,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testFindAllIPrologTermArray() {
+	public final void testFindAllPrologTermArray() {
 
 		engine.assertz("parent( pam, bob)");
 		engine.assertz("parent( tom, bob)");
@@ -995,7 +995,7 @@ public class PrologEngineTest extends PrologBaseTest {
 	}
 
 	@Test
-	public final void testCreateQueryIPrologTermArray() {
+	public final void testCreateQueryPrologTermArray() {
 
 		// employee relationship
 		engine.assertz("employee( mcardon, 1, 5 )");
